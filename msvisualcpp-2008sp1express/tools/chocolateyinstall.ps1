6e999d69df3e3ad937c375a70c2008fb95d0bd2a
Edward J. Schwartz <eschwartz@cert.org>
2021-05-07 16:36:38 -0400

    Add vs2010

--- msvisualcpp-2008sp1express/tools/chocolateyinstall.ps1
+++ msvisualcpp-2008sp1express/tools/chocolateyinstall.ps1
@@ -4,5 +4,5 @@ $url        = 'https://download.microsoft.com/download/E/8/E/E8EEB394-7F42-4963-
 
 Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$env:temp\$packageName.iso" -Checksum "DBEBC84C35435C2B5D33F61D9A269D56C100FDDC8BDB63D3456CB05666B75013544E04DD903E27619938D25F90E18A8FAA17895055B1402CB3FAA7B4F292C172" -ChecksumType sha512 -Url "$url"
 imdisk -a -f "$env:temp\$packageName.iso" -m "w:"
-Install-ChocolateyInstallPackage "$packageName" 'exe' '/qb /norestart /log $env:temp\vs.log' 'w:\VCExpress\setup.exe'
+Install-ChocolateyInstallPackage "$packageName" 'exe' '/q /norestart /log $env:temp\vs.log' 'w:\VCExpress\setup.exe'
 imdisk -d -m w:
---
+++ msvisualcpp-2010express/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://archive.org/download/vs-2010-express-1/VS2010Express1.iso'
+
+Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$env:temp\$packageName.iso" -Checksum "4B9A07CC14FF93DD5ADAE24FB672A7CCC769F03A6ABECCBFD936827CC2CC3F3D1913E4CFF22E34E318442B0A048B47BF1A374DF4EF85AF72406282B06284B0B4" -ChecksumType sha512 -Url "$url"
+imdisk -a -f "$env:temp\$packageName.iso" -m "w:"
+Install-ChocolateyInstallPackage "$packageName" 'exe' '/q /norestart /log $env:temp\vs.log' 'w:\VCExpress\setup.exe'
+imdisk -d -m w:
