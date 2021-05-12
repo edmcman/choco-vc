d1c7461a44069a985372418985be210e49f71376
Edward J. Schwartz <eschwartz@cert.org>
2021-05-12 09:57:52 -0400

    imdisk does not seem to work

--- msvisualstudio-2013express/tools/chocolateyinstall.ps1
+++ msvisualstudio-2013express/tools/chocolateyinstall.ps1
@@ -2,7 +2,16 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://download.microsoft.com/download/7/2/E/72E0F986-D247-4289-B9DC-C4FB07374894/VS2013_RTM_DskExp_ENU.iso'
 
-Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$env:temp\$packageName.iso" -Checksum "C655554D3CA62DC2105A146EFB4FD3BAE3237FF1CBB38667C3FE1CBFB9C0D950A3BD88DEBDFAAE6F6475C4A647EA41361F3D3426E7762A50A62FC174E70C1381" -ChecksumType sha512 -Url "$url"
-imdisk -a -f "$env:temp\$packageName.iso" -m "w:"
-Install-ChocolateyInstallPackage "$packageName" 'exe' "/q /norestart /log $env:temp\vs.log" 'w:\wdexpress_full.exe'
-imdisk -D -m w:
+$iso = "$env:temp\$packageName.iso"
+Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$iso" -Checksum "C655554D3CA62DC2105A146EFB4FD3BAE3237FF1CBB38667C3FE1CBFB9C0D950A3BD88DEBDFAAE6F6475C4A647EA41361F3D3426E7762A50A62FC174E70C1381" -ChecksumType sha512 -Url "$url"
+
+Mount-DiskImage -ImagePath $iso
+
+#Get the drive letter where iso is mounted
+$driveLetter = (Get-DiskImage $iso | Get-Volume).DriveLetter
+
+# Run commands against the mounted ISO, in this case, run the setup.exe
+Install-ChocolateyInstallPackage "$packageName" 'exe' "/q /norestart /log $env:temp\vs.log" '${driveLetter}:\wdexpress_full.exe'
+
+# Unmount the ISO file when finished
+Dismount-DiskImage -ImagePath $iso
