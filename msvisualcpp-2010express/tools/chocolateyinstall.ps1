33eb15929d3106b1335839c11ee94d723ace7101
Edward J. Schwartz <eschwartz@cert.org>
2021-05-13 11:03:48 -0400

    Get rid of imdisk completely.

--- msvisualcpp-2010express/tools/chocolateyinstall.ps1
+++ msvisualcpp-2010express/tools/chocolateyinstall.ps1
@@ -2,7 +2,16 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://archive.org/download/vs-2010-express-1/VS2010Express1.iso'
 
-Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$env:temp\$packageName.iso" -Checksum "4B9A07CC14FF93DD5ADAE24FB672A7CCC769F03A6ABECCBFD936827CC2CC3F3D1913E4CFF22E34E318442B0A048B47BF1A374DF4EF85AF72406282B06284B0B4" -ChecksumType sha512 -Url "$url"
-imdisk -a -f "$env:temp\$packageName.iso" -m "w:"
-Install-ChocolateyInstallPackage "$packageName" 'exe' "/q /norestart /log $env:temp\vs.log" 'w:\VCExpress\setup.exe'
-imdisk -d -m w:
+$iso = "$env:temp\$packageName.iso"
+Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$iso" -Checksum "4B9A07CC14FF93DD5ADAE24FB672A7CCC769F03A6ABECCBFD936827CC2CC3F3D1913E4CFF22E34E318442B0A048B47BF1A374DF4EF85AF72406282B06284B0B4" -ChecksumType sha512 -Url "$url"
+
+Mount-DiskImage -ImagePath $iso
+
+#Get the drive letter where iso is mounted
+$driveLetter = (Get-DiskImage $iso | Get-Volume).DriveLetter
+
+# Run commands against the mounted ISO, in this case, run the setup.exe
+Install-ChocolateyInstallPackage "$packageName" 'exe' "/q /norestart /log $env:temp\vs.log" "${driveLetter}:\wdexpress_full.exe"
+
+# Unmount the ISO file when finished
+Dismount-DiskImage -ImagePath $iso
--- msvisualstudio-2012express/tools/chocolateyinstall.ps1
+++ msvisualstudio-2012express/tools/chocolateyinstall.ps1
@@ -2,7 +2,16 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://download.microsoft.com/download/1/F/5/1F519CC5-0B90-4EA3-8159-33BFB97EF4D9/VS2012_WDX_ENU.iso'
 
-Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$env:temp\$packageName.iso" -Checksum "516DACC4A5310971532AA37CEC60DFB4DD62993F54D7E787FFC83D6CC4CF78AF244E064AD3C6B7D6F3FA8B21A5BE2D7112F2AE5EC462707F7A12D9123E44BC03" -ChecksumType sha512 -Url "$url"
-imdisk -a -f "$env:temp\$packageName.iso" -m "w:"
-Install-ChocolateyInstallPackage "$packageName" 'exe' "/q /norestart /log $env:temp\vs.log" 'w:\wdexpress_full.exe'
-imdisk -D -m w:
+$iso = "$env:temp\$packageName.iso"
+Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$iso" -Checksum "516DACC4A5310971532AA37CEC60DFB4DD62993F54D7E787FFC83D6CC4CF78AF244E064AD3C6B7D6F3FA8B21A5BE2D7112F2AE5EC462707F7A12D9123E44BC03" -ChecksumType sha512 -Url "$url"
+
+Mount-DiskImage -ImagePath $iso
+
+#Get the drive letter where iso is mounted
+$driveLetter = (Get-DiskImage $iso | Get-Volume).DriveLetter
+
+# Run commands against the mounted ISO, in this case, run the setup.exe
+Install-ChocolateyInstallPackage "$packageName" 'exe' "/q /norestart /log $env:temp\vs.log" "${driveLetter}:\wdexpress_full.exe"
+
+# Unmount the ISO file when finished
+Dismount-DiskImage -ImagePath $iso
