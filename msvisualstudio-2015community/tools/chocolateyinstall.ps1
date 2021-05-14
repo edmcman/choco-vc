f92c7134f4630c768fd05fc0664def1614fac812
Edward J. Schwartz <eschwartz@cert.org>
2021-05-13 20:05:07 -0400

    Switch to VS2015 community edition.

--- msvisualstudio-2015community/tools/chocolateyinstall.ps1
+++ msvisualstudio-2015community/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.my.visualstudio.com/pr/en_visual_studio_express_2015_for_windows_desktop_x86_dvd_cddc01f6.iso?t=981e2fb5-6cf8-4592-bb3e-f30fb6c5b5c8&e=1620866779&h=32d3d7c15444d70b54d3e47ae1c3c09a&su=1'
+$url        = 'https://go.microsoft.com/fwlink/?LinkId=615448&clcid=0x409'
 
 $iso = "$env:temp\$packageName.iso"
 Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$iso" -Checksum "618703727411BE10F06266CA4932A2D9BD3FA9664C069EB1DBA6EC25F22DDE84401DFE23BFCC9ABD419439AC6DA2158A4F8F2A7365E4CF843E8E32A74CA5F184" -ChecksumType sha512 -Url "$url"
